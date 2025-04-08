9dfc23231b4af877079f02c6fdd6febb194610f8
rkc137 <hlopov100@gmail.com>
2025-04-08 21:21:23 +0300

    add update

--- src/ui/label/label.cpp
+++ src/ui/label/label.cpp
@@ -6,16 +6,39 @@ namespace ui
 Label::Label(const sf::String str_text, 
             const sf::Font &font,
             const sf::Color &color,
-            unsigned int ch_size)
-            : Label({font, str_text, ch_size}, color)
+            unsigned int ch_size,
+            OriginState state)
+            : Label({font, str_text, ch_size}, color, state)
 {}
 
-Label::Label(sf::Text &&text, const sf::Color &color)
-    : text(text)
+Label::Label(sf::Text &&text, const sf::Color &color, OriginState state)
+    : text(text), origin_state(state)
 {
     text.setFillColor(color);
     update_origin();
     text.setPosition({0, 0});
+}
+
+void Label::set_string(sf::String &&str)
+{
+    text.setString(std::move(str));
+    update_origin();
+}
+
+sf::String Label::get_string()
+{
+    return text.getString();
+}
+
+void Label::set_origin_state(OriginState state)
+{
+    origin_state = state;
+    update_origin();
+}
+
+Label::OriginState Label::get_origin_state()
+{
+    return origin_state;
 }
 
 void Label::set_char_size(unsigned int ch_size)
@@ -44,10 +67,23 @@ void Label::update_origin()
     sf::FloatRect text_rect = text.getLocalBounds();
     auto pos = text_rect.position;
     auto size = text_rect.size;
-    text.setOrigin({
-        pos.x + size.x / 2.0f,
-        pos.y + size.y / 2.0f
-    });
+    switch(origin_state)
+    {
+    case OriginState::center:
+        text.setOrigin({
+            pos.x + size.x / 2.0f,
+            pos.y + size.y / 2.0f
+        });
+        break;
+    case OriginState::left_up:
+        text.setOrigin({
+            pos.x,
+            pos.y
+        });
+        break;
+    default:
+        break;
+    }
 }
 
 void Label::draw(sf::RenderTarget& target, sf::RenderStates states) const
--- src/ui/label/label.hpp
+++ src/ui/label/label.hpp
@@ -11,13 +11,26 @@ namespace ui
 class Label : public BaseWidget<>
 {
 public:
+    enum class OriginState
+    {
+        center,
+        left_up
+    };
+
     explicit Label(const sf::String str_text = "", 
                     const sf::Font &font = res::default_font,
                     const sf::Color &color = sf::Color::Black,
-                    unsigned int ch_size = 30);
-               
-    explicit Label(sf::Text&& text, const sf::Color &color = sf::Color::Black);
-
+                    unsigned int ch_size = 30,
+                    OriginState state = OriginState::center);
+    
+    explicit Label(sf::Text&& text,
+                    const sf::Color &color = sf::Color::Black,
+                    OriginState state = OriginState::center);
+    
+    void set_string(sf::String &&str);
+    sf::String get_string();
+    void set_origin_state(OriginState state);
+    OriginState get_origin_state();
     void set_char_size(unsigned int ch_size);
     unsigned int get_char_size();
     void set_fill_color(sf::Color color);
@@ -28,6 +41,7 @@ public:
     void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 
 private:
+    OriginState origin_state;
     sf::Text text;
 };
 
