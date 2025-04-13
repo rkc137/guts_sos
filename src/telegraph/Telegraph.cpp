09f82e360a95072abf03a998e2aed44314333e73
rkc137 <hlopov100@gmail.com>
2025-04-13 23:58:25 +0300

    trying ti implement Telegraph

--- src/telegraph/Telegraph.cpp
+++ src/telegraph/Telegraph.cpp
@@ -1,41 +1,51 @@
 #include "Telegraph.hpp"
 
-Telegraph::Telegraph()
+Telegraph::Telegraph() : button(*this)
 {
 }
 
-Telegraph::AllwaysFocusButton::AllwaysFocusButton()
-    : ui::LabelButton{ui::Label{}} 
+Telegraph::AllwaysFocusButton::AllwaysFocusButton(Telegraph &parent)
+    : ui::LabelButton{ui::Label{}}, parent(parent)
 {
     key_button = sf::Keyboard::Scancode::Space;
     is_in_focus = true;
+    press_noise.setLooping(true);
     on_press = [&](){
+        clock_since_press.restart();
+        press_noise.play();
+        dbg << "press";
+    };
+    on_release = [&](){
+        auto time_since_press = clock_since_press.getElapsedTime();
+        dbg << "release";
+        press_noise.stop();
     };
 }
 
 void Telegraph::update([[maybe_unused]] double delta_time)
 {
+    button.update(delta_time);
 }
 
 void Telegraph::resize()
 {
     auto ws = get_window().getSize();
-    input.set_char_size(ws.y / 10);
-    output.set_char_size(ws.y / 10);
-    input.setPosition({
+    input_label.set_char_size(ws.y / 10);
+    output_label.set_char_size(ws.y / 10);
+    input_label.setPosition({
         ws.x / 2.f,
         ws.y / 2.f
     });
-    output.setPosition({
+    output_label.setPosition({
         ws.x / 2.f,
-        ws.y / 2.f + input.get_global_bounds().size.y * 1.5f
+        ws.y / 2.f + input_label.get_global_bounds().size.y * 1.5f
     });
 }
 
 void Telegraph::draw(sf::RenderTarget &target, sf::RenderStates states) const
 {
-    target.draw(input, states);
-    target.draw(output, states);
+    target.draw(input_label, states);
+    target.draw(output_label, states);
 }
 
 
--- src/telegraph/Telegraph.hpp
+++ src/telegraph/Telegraph.hpp
@@ -2,6 +2,8 @@
 
 #include <SFML/Window.hpp>
 
+#include "../logger/Logger.hpp"
+
 #include "../scenes/SceneManager.hpp"
 #include "../ui/label/label.hpp"
 #include "../ui/Buttons/LabelButton.hpp"
@@ -13,16 +15,22 @@ public:
     void update(double delta_time) override;
     void resize() override;
     void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
-    ui::Label input{L"1___1", res::cybersomething, sf::Color::White, 80, ui::Label::OriginState::center};//••—•——
-    ui::Label output{input};//text
+    ui::Label input_label{L"1___1", res::cybersomething, sf::Color::White, 80, ui::Label::OriginState::center};//••—•——
+    ui::Label output_label{input_label};//text
+    sf::String input;
+    sf::String output;
 private:
-    sf::Time dash_time = sf::seconds(0.25);
-    sf::Time letter_time = sf::seconds(1.70);
-    sf::Time word_time = sf::seconds(2);
+    inline static const sf::Time dash_time = sf::seconds(0.25);
+    inline static const sf::Time letter_time = sf::seconds(1.70);
+    inline static const sf::Time word_time = sf::seconds(2);
     
     class AllwaysFocusButton : public ui::LabelButton
     {
     public: 
-        AllwaysFocusButton();
+        AllwaysFocusButton(Telegraph &parent);
+        sf::Sound press_noise{res::morse_noise};
+        sf::Clock clock_since_press;
+        // sf::Clock last_press_clock;
+        Telegraph &parent;
     } button;
 };
