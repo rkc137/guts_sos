c1d3128b8a37b059d188fa0b2f59d6e86e266f53
rkc137 <hlopov100@gmail.com>
2025-05-09 09:34:29 +0300

    some pretty to main

--- src/main.cpp
+++ src/main.cpp
@@ -21,25 +21,29 @@ int main()
         auto scene = SceneManager::get_current();
             
         while(const std::optional event = window.pollEvent())
-            if(event->is<sf::Event::Closed>())
-                window.close();
+        {
+            if(event->is<sf::Event::Closed>()) window.close();
             else if(const auto* resized = event->getIf<sf::Event::Resized>())
             {
                 auto size = resized->size; 
                 window.setView(sf::View{{size.x / 2.f, size.y / 2.f}, sf::Vector2f{size}});
                 scene->resize();
             }
+        }
+
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::F11))
-            window.change_fullscreen(),
+        {
+            window.change_fullscreen();
             scene->resize();
+        }
 
         scene->update(delta_time);
         window.draw(*scene);
         window.display();
         window.clear();
         
-        if(auto fps = 1 / delta_time; fps < 24)
-            dbg << fps;
+        // if(auto fps = 1 / delta_time; fps < 24)
+        //     dbg << fps;
     }
     
     return 0;
