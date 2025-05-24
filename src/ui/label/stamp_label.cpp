63d6c1539a25d98daba1cbc3fad383a0921599f0
rkc137 <hlopov100@gmail.com>
2025-05-24 17:13:48 +0300

    add StampLabelMusic

--- src/ui/label/stamp_label.cpp
+++ src/ui/label/stamp_label.cpp
@@ -39,4 +39,39 @@ void StampLabelSound::update(unused double delta_time)
     clock.restart();
 }
 
+StampLabelMusic::StampLabelMusic(
+    Label &&label,
+    sf::String text,
+    sf::Music &music,
+    sf::Time letter_time) 
+    : BaseStampLabel(std::move(label), text, letter_time),
+      voice(music)
+{
+}
+
+StampLabelMusic::~StampLabelMusic()
+{
+    voice.pause();
+}
+
+void StampLabelMusic::update(unused double delta_time)
+{
+    if(is_done() && first_on_end)
+    {
+        first_on_end = false;
+        voice.pause();
+        return;
+    }
+    else if(is_done() || clock.getElapsedTime() < letter_time)
+        return;
+    else if(is_on_start)
+    {
+        is_on_start = false;
+        voice.play();
+    }
+    append_string(stamp_text[stamp_iter]);
+    ++stamp_iter;
+    clock.restart();
+}
+
 }
--- src/ui/label/stamp_label.hpp
+++ src/ui/label/stamp_label.hpp
@@ -38,4 +38,21 @@ private:
     std::size_t sounds_iter = 0;
 };
 
+class StampLabelMusic : public BaseStampLabel
+{
+public:
+    explicit StampLabelMusic(
+        Label&& label,
+        sf::String text,
+        sf::Music &voice,
+        sf::Time letter_time = sf::seconds(0.2)
+    );
+    ~StampLabelMusic();
+    void update(double delta_time) override;
+private:
+    bool is_on_start = true;
+    bool first_on_end = true;
+    sf::Music &voice;
+};
+
 }
